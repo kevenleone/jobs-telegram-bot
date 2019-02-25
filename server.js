5dca8ade4dcf1d0ceb1b7a800b06113a78024518
Keven Leone <keven.santos.sz@gmail.com>
2019-02-25 12:21:45 -0300

    Adicionado rota /

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const server = require('./config/loader');
 const PORT = process.env.PORT || 3000;
 
+server.get('/', (req, res) => res.send('Running !!!'));
+
 server.listen(PORT, () => console.log(`Running on ${PORT}`));
